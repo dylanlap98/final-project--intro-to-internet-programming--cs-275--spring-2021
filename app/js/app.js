755486849ac7ee8c44c6cf745a7e5f0867c572a3
Dylan LaPierre <56058450+dylanlap98@users.noreply.github.com>
2021-05-11 13:21:06 -0400

    Remove text from CSS and Add # element for diamond-container

--- app/js/app.js
+++ app/js/app.js
@@ -2,3 +2,8 @@ window.onload = () => {
     console.log(`%c JavaScript works!`,
         `background-color: yellow; font-weight: bold;`);
 };
+
+let number = parseInt(window.prompt('Please input a number:'));
+while(isNaN(number) === true || number <= 0) {
+    number = parseInt(window.prompt('Please input a number:'))
+}
