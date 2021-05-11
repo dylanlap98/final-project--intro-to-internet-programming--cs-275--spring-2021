b4d1d0f93a6ded91fb4e21cae5e9776435e21b9c
Dylan LaPierre <56058450+dylanlap98@users.noreply.github.com>
2021-05-11 13:45:48 -0400

    Add move func and css changes to move diamond box

--- app/js/app.js
+++ app/js/app.js
@@ -7,3 +7,15 @@ let number = parseInt(window.prompt('Please input a number:'));
 while(isNaN(number) === true || number <= 0) {
     number = parseInt(window.prompt('Please input a number:'))
 }
+
+let diamondBox = document.getElementById('diamond-container');
+
+// timer variable using funciton move
+let timer = window.setInterval(move, 1000);
+
+//moves diamond box by 10 px.
+function move(){
+    diamondBox.style.left = 10 + 'px';
+}
+
+
