1210c2ea4f55494c4ba2d0fac155516f241f43c1
Dylan LaPierre <56058450+dylanlap98@users.noreply.github.com>
2021-05-12 15:33:07 -0400

    Add movement to box

--- app/js/app.js
+++ app/js/app.js
@@ -1,21 +1,21 @@
-window.onload = () => {
-    console.log(`%c JavaScript works!`,
-        `background-color: yellow; font-weight: bold;`);
-};
-
+//popup for requesting number of diamond size
 let number = parseInt(window.prompt('Please input a number:'));
 while(isNaN(number) === true || number <= 0) {
     number = parseInt(window.prompt('Please input a number:'))
 }
 
 let diamondBox = document.getElementById('diamond-container');
-
 // timer variable using funciton move
-let timer = window.setInterval(move, 1000);
+//let timer = window.setInterval(move, 1000);
 
-//moves diamond box by 10 px.
-function move(){
-    diamondBox.style.left = 10 + 'px';
-}
+//moves diamond box by 20 px.
+// function move(){
+//     diamondBox.style.left = 20 + 'px';
+// }
+
+window.setInterval(function(){
+    let leftSide = diamondBox.offsetLeft
+    diamondBox.style.left = (leftSide + 5) + 'px';
+}, 10)
 
 
